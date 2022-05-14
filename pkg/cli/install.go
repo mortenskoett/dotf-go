84d3fb12534d8f1ce2b91a3388917c90322b5dfe
Morten Skøtt <mortenskoett@gmail.com>
2022-05-14 11:07:02 +0200

    Update install command to call backend

--- pkg/cli/install.go
+++ pkg/cli/install.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/mortenskoett/dotf-go/pkg/config"
+	"github.com/mortenskoett/dotf-go/pkg/terminalio"
 )
 
 type installCommand struct {
@@ -22,13 +23,12 @@ func (c *installCommand) Run(args *CliArguments, conf *config.DotfConfiguration)
 		return err
 	}
 
-	// filepath := args.PosArgs[0]
+	filepath := args.PosArgs[0]
 
-	// TODO
-	// err := terminalio.installDotfile(filepath, conf.HomeDir, conf.DotfilesDir)
-	// if err != nil {
-	// 	return err
-	// }
+	err := terminalio.InstallDotfile(filepath, conf.HomeDir, conf.DotfilesDir)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -38,12 +38,12 @@ func (c *installCommand) CmdName() string {
 }
 
 func (c *installCommand) Overview() string {
-	return "Installs a dotfile into the userspace by symlinking to it from userspace."
+	return "Installs an existing dotfile into userspace by symlinking to the dotfile from userspace."
 }
 
 func (c *installCommand) Arguments() []Arg {
 	return []Arg{
-		{Name: "file/dir", Description: "Path to file or dir inside dotfiles to install or path to userspace file to overwrite."},
+		{Name: "file/dir", Description: "Path to file or dir inside dotfiles or path to file or dir in userspace."},
 	}
 }
 
@@ -56,8 +56,17 @@ func (c *installCommand) Description() string {
 	Will install a file or directory from dotfiles into the same location in userspace. If an
 	identically named file is found in userspace a prompt will ask whether to delete the file or
 	abort the operation. The only sane thing is to remove the file in userspace as the idea is for
-	the previously created dotfile to take its place. The command can be used both on files inside
-	the dotfiles directory as well as files in userspace and will do the same thing. `
+	the previously created dotfile to take its place.
+
+	The command can be used both on files inside the dotfiles directory as well as files in
+	userspace and will do the same thing. A file inside dotfiles can be considered the source and
+	a file in userspace will be considered the target. The source must exist and the target will be
+	overwritten (after a backup is made).
+
+	- If a file from inside dotfiles is given this file will be used as installation source.
+	- If a file from userspace is given this file will be used as target.
+	- The command performs the same operation in both cases.
+	`
 }
 
 func (c *installCommand) ProgName() string {
